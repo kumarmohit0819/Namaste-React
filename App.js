76d1a8ad7fbf2aea0bff7459965dd2df2b9dce21
Mohit Kumar <kumarmohit0819@gmail.com>
2023-04-05 13:18:54 +0530

    ignite our app

--- App.js
+++ App.js
@@ -1,32 +1,6 @@
-/*******Single Element structure***** */
-// const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
-// console.log(heading);
+import React  from "react";
+import ReactDOM from "react-dom/client";
 
-/*********************** Nested element structure********/
-{
-  /* <div id="parent">
-    <div id= "child">
-        <h1>I'm an h1 tag</h1>
-    </div>
-</div> */
-}
-
-// const heading = React.createElement("div", {id:"parent"},React.createElement("div", {id:"child"}, React.createElement("h1",{}, "I'm an h1 tag")));
-
-/***************Parallel element *****************/
-
-{
-  /* <div id="parent">
-    <div id= "child">
-        <h1>I'm an h1 tag</h1>
-        <h2>I'm an h2 tag</h2>
-    </div>
-     <div id= "child2">
-        <h1>I'm an h1 tag</h1>
-        <h2>I'm an h2 tag</h2>
-    </div>
-</div> */
-}
 
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namaste React 
+# Namaste React 
+
+# Parcel
+ - Dev Build
+ - Local Server
+ - HMR = Hot Module Replacement
+ - File Watching Algorithm - written in c++
+ - Caching - Faster Build
+ - Image Optimization
+ - Minification
+ - Bundling
+ - Compressing
+ - Consistent Hashing
+ - Code Splitting
+ - Differential Bundling - support older browser
+ - Diagnostic
+ - Error Handling
+ - Https
+ - Tree Shaking =  remove unused code
+ - Different dev and prod bundles
